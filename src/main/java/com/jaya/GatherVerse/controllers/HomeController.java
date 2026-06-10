e49ffe5c77b031fc50df579550d0386dd96a8c05
Jayavarshini <varshusharvesh1234@gmail.com>
2026-06-10 10:43:13 +0530

    Add HomeController to redirect root URL to login page

---
+++ src/main/java/com/jaya/GatherVerse/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.jaya.GatherVerse.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/login/";
+    }
+}
